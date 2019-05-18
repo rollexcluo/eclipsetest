a050181777790a0f85c82f77e0f88c9fc2098d04
cluo_glb <rollex_glb@test.com>
2019-05-18 14:10:23 +0800

    happy

---
+++ src/main/java/com/cluo/git/Happy.java
@@ -0,0 +1,10 @@
+package com.cluo.git;
+
+public class Happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("I'm very happy");
+	}
+
+}
